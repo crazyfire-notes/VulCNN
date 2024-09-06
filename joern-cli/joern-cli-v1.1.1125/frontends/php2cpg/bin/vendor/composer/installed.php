cc3de47422a7d2d4b2daa9da4cc229c51abfe6c5
Crazyfire Lee <me@crazyfirelee.tw>
2024-09-06 09:40:56 +0800

    add joern-cli

---
+++ joern-cli/joern-cli-v1.1.1125/frontends/php2cpg/bin/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '181d39e50847facbcbeb7711c8de95882bfb3616',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '181d39e50847facbcbeb7711c8de95882bfb3616',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nikic/php-parser' => array(
+            'pretty_version' => 'v4.13.2',
+            'version' => '4.13.2.0',
+            'reference' => '210577fe3cf7badcc5814d99455df46564f3c077',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/php-parser',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
